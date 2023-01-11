abae5101d5a6df9495e7303dc297047c9462ebfb
enpitsulin <enpitsulin@gmail.com>
2023-01-11 10:43:42 +0800

    react-swc hmr in vps

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -8,6 +8,7 @@ function Page() {
     <>
       <h1>Welcome</h1>
       This page is:
+      <p>change /pages/index/index.page.tsx to test HMR</p>
       <ul>
         <li>Rendered to HTML.</li>
         <li>
